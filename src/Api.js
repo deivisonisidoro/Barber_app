422740b0688e2859a53a2c92f2f4329db3220db4
deivisonisidoro <deivisonisidoro@gmail.com>
2021-01-17 14:39:37 -0300

    feat: Listando os barbeiros da area

--- src/Api.js
+++ src/Api.js
@@ -1,4 +1,7 @@
+import AsyncStorage from '@react-native-community/async-storage';
+
 const BASE_API = 'https://api.b7web.com.br/devbarber/api';
+
 export default {
   checkToken: async ( token) =>{
     const req  = await fetch(`${BASE_API}/auth/refresh`, {
@@ -36,4 +39,16 @@ export default {
   const json = await req.json();        
   return json;
 },
+  getBarbers: async (lat=null, lng=null, address=null) => {
+    const token = await AsyncStorage.getItem('token');
+
+    console.log("LAT", lat);
+    console.log("LNG", lng);
+    console.log("ADDRESS", address);
+
+    const req = await fetch(`${BASE_API}/barbers?token=${token}&lat=${lat}&lng=${lng}&address=${address}`);
+    const json = await req.json();
+    return json;
+  }
+
 }
